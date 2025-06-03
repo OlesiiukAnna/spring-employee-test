c0455d61998b61b863c0a8102705d994f91eb3e1
Anna Olesiiuk <olesiiuk.anna@gmail.com>
2025-06-03 12:51:31 +0300

    initial commit

---
+++ src/main/java/com/test/springboot/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.test.springboot.service;
+
+import java.util.List;
+
+import com.test.springboot.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+	
+}
